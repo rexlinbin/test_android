0aa557026414aadfe032c945eca4953594b94ea0
bin.lin <bin.lin@tendcloud.com>
2019-03-14 14:16:24 +0800

    修复bug

--- app/src/main/java/com/test/test/MainActivity.java
+++ app/src/main/java/com/test/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.test.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lengyue.test.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
